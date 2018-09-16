6321f5d6eafc8e0c5512472d8cc7f5acaaf5b56e
Massimo Biolcati <massimobio@gmail.com>
2018-09-16 18:02:47 -0400

    Remove unused code

--- ProgressHUD/ProgressHUD/ProgressHUD.swift
+++ ProgressHUD/ProgressHUD/ProgressHUD.swift
@@ -171,9 +171,6 @@ class ProgressHUD: NSView {
         }
     }
 
-    /// The minimum size of the HUD.
-    var minSize = CGSize.zero
-
     /// Force the HUD dimensions to be equal if possible.
     var square = false
 
@@ -633,12 +630,6 @@ class ProgressHUD: NSView {
                 totalSize.height = maximum
             }
         }
-        if totalSize.width < minSize.width {
-            totalSize.width = minSize.width
-        }
-        if totalSize.height < minSize.height {
-            totalSize.height = minSize.height
-        }
         size = totalSize
     }
 
@@ -717,77 +708,6 @@ class ProgressHUD: NSView {
 
 }
 
-class RoundProgressView: NSView {
-
-    @objc var progress: CGFloat = 0.0 { didSet { needsDisplay = true } }
-    var progressTintColor = NSColor.black { didSet { needsDisplay = true } }
-    var annular = false { didSet { needsDisplay = true } } // Display mode - false = circular or true = annular.
-
-    convenience init() {
-        self.init(frame: CGRect(x: 0.0, y: 0.0, width: 37.0, height: 37.0))
-    }
-
-    override init(frame: NSRect) {
-        super.init(frame: frame)
-        layer?.backgroundColor = NSColor.clear.cgColor
-        layer?.isOpaque = false
-    }
-
-    required init?(coder decoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func draw(_ rect: NSRect) {
-
-        let allRect = bounds
-        let circleRect = allRect.insetBy(dx: 2.0, dy: 2.0)
-
-        guard let context = NSGraphicsContext.current?.cgContext else { return }
-
-        if annular {
-
-            let lineWidth: CGFloat = 5.0
-            let processBackgroundPath = NSBezierPath()
-            processBackgroundPath.lineWidth = lineWidth
-            processBackgroundPath.lineCapStyle = .round
-
-            // Draw progress
-            let center = CGPoint(x: bounds.size.width / 2, y: bounds.size.height / 2)
-            let radius = (bounds.size.width - lineWidth) / 2
-            let startAngle: CGFloat = 90
-            var endAngle = startAngle - 360 * progress
-            processBackgroundPath.appendArc(withCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
-            progressTintColor.set()
-            processBackgroundPath.stroke()
-            let processPath = NSBezierPath()
-            processPath.lineCapStyle = .round
-            processPath.lineWidth = lineWidth
-            endAngle = startAngle - .pi * 2
-            processPath.appendArc(withCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
-            progressTintColor.set()
-            processPath.stroke()
-
-        } else {
-
-            progressTintColor.setStroke()
-            context.setLineWidth(2.0)
-            context.strokeEllipse(in: circleRect)
-
-            // Draw progress
-            let center = CGPoint(x: allRect.size.width / 2, y: allRect.size.height / 2)
-            let radius = (allRect.size.width - 4) / 2
-            let startAngle: CGFloat = .pi / 2
-            let endAngle = startAngle - .pi * 2 * progress
-            context.setFillColor(progressTintColor.cgColor)
-            context.move(to: CGPoint(x: center.x, y: center.y))
-            context.addArc(center: CGPoint(x: center.x, y: center.y), radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
-            context.closePath()
-            context.fillPath()
-        }
-    }
-
-}
-
 class ProgressIndicatorLayer: CALayer {
 
     private(set) var isRunning = false
@@ -827,7 +747,7 @@ class ProgressIndicatorLayer: CALayer {
     var fadeDownOpacity: CGFloat = 0.0
     var numFins = 12
     var finLayers = [CALayer]()
-    
+
     init(size: CGFloat) {
         super.init()
         bounds = CGRect(x: -(size / 2), y: -(size / 2), width: size, height: size)
@@ -836,11 +756,11 @@ class ProgressIndicatorLayer: CALayer {
             setupAnimTimer()
         }
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     deinit {
         stopProgressAnimation()
         removeFinLayers()
